3391e965e3a7526a79731caf1f5876793b93f245
Geovani566 <geovanitomaz3@gmail.com>
2025-11-04 12:46:13 -0300

    Primeiro commit do projeto Dart

---
+++ bin/curso_de_dart_udemy.dart
@@ -0,0 +1,5 @@
+import 'package:curso_de_dart_udemy/curso_de_dart_udemy.dart' as curso_de_dart_udemy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_de_dart_udemy.calculate()}!');
+}
---
+++ test/curso_de_dart_udemy_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_de_dart_udemy/curso_de_dart_udemy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

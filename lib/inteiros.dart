666e782025980d9c79dcbff8b470a68c6667de9c
Geovani566 <geovanitomaz3@gmail.com>
2025-11-05 13:26:21 -0300

    aula de inteiros

---
+++ lib/inteiros.dart
@@ -0,0 +1,13 @@
+void main(){
+   int altura = 183;
+   print("altura $altura");
+
+   int ano = 2025;
+   print("ano $ano");
+
+   int idade = 20;
+   print("idade $idade");
+
+   int datadenascimento = ano - idade;
+   print(datadenascimento);
+}
